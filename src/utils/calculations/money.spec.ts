25b6e9057d08d05eedd17ad17d93ea8900e3545d
Paul <paulglujan@gmail.com>
2022-12-20 11:21:38 -0800

    Wrote tests for money.ts

---
+++ src/utils/calculations/money.spec.ts
@@ -0,0 +1,11 @@
+import { getPriceStringFromCents } from './money';
+
+describe('Test getPriceFromCents', () => {
+    test('Cents converted to money string', () => {
+        expect(getPriceStringFromCents(1500)).toBe('$15.00');
+    });
+
+    test('Empty string when no input', () => {
+        expect(getPriceStringFromCents()).toBe('');
+    });
+});

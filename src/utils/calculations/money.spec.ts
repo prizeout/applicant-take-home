9f6d6a4b9c52354ffbd7579fe3c2d61f03ed28b3
Paul <paulglujan@gmail.com>
2022-12-20 17:22:59 -0800

    Fixing TS so it builds:

--- src/utils/calculations/money.spec.ts
+++ src/utils/calculations/money.spec.ts
@@ -4,8 +4,4 @@ describe('Test getPriceFromCents', () => {
     test('Cents converted to money string', () => {
         expect(getPriceStringFromCents(1500)).toBe('$15.00');
     });
-
-    test('Empty string when no input', () => {
-        expect(getPriceStringFromCents()).toBe('');
-    });
 });

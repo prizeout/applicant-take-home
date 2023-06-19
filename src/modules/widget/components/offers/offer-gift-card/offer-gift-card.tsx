26a49705a701d46c58d24956e2ce2a2740b0c0a8
Dain Kang <dainkang1@gmail.com>
2023-06-19 16:13:15 -0400

    toggle border on giftcard click

--- src/modules/widget/components/offers/offer-gift-card/offer-gift-card.tsx
+++ src/modules/widget/components/offers/offer-gift-card/offer-gift-card.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Classnames from 'classnames';
 import { GiftCard, BonusTag } from '../../../../../components/common/';
 import { PrizeoutOffer } from '../../../../../slices/offers-slice';
@@ -12,12 +12,13 @@ interface OfferGiftCardProps {
 
 export const OfferGiftCard: React.FC<OfferGiftCardProps> = ({ offer, onClickHandler }): React.ReactElement => {
     let activeOfferId;
+    const [isSelected, setIsSelected] = useState(false);
 
     const firstGiftCard = offer.giftcard_list[0];
     const offerType = firstGiftCard.display_monetary_bonus ? 'monetary' : 'percentage';
     const offerValue = firstGiftCard.display_bonus;
     const classes: string = Classnames('offer-gift-card', {
-        'offer-gift-card--selected': activeOfferId === firstGiftCard.checkout_value_id,
+        'offer-gift-card--selected': activeOfferId === firstGiftCard.checkout_value_id || isSelected,
     });
 
     const selectOfferOnEnter = (event: React.KeyboardEvent<HTMLElement>) => {
@@ -26,10 +27,15 @@ export const OfferGiftCard: React.FC<OfferGiftCardProps> = ({ offer, onClickHand
         }
     };
 
+    const clickOffer = () => {
+        setIsSelected(!isSelected);
+        onClickHandler();
+    };
+
     return (
         <div
             className={classes}
-            onClick={() => onClickHandler()}
+            onClick={() => clickOffer()}
             onKeyDown={(event) => selectOfferOnEnter(event)}
             role="button"
             tabIndex={0}

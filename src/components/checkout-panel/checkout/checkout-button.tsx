7a7ad4d17670994ae3cbdf50186d84532314f3a7
Dain Kang <dainkang1@gmail.com>
2023-06-22 22:18:36 -0400

    mock api call on prizeout click

--- src/components/checkout-panel/checkout/checkout-button.tsx
+++ src/components/checkout-panel/checkout/checkout-button.tsx
@@ -1,10 +1,50 @@
 import React from 'react';
 import { Button } from '../../common';
+import { PrizeoutOfferValueOptions } from '../../../slices/offers-slice';
 
-const CheckoutButton: React.FC = (): React.ReactElement => {
+interface CheckoutRedemptionProps {
+    selectedValue?: PrizeoutOfferValueOptions;
+    giftCardName?: string;
+    
+}
+
+interface Payload {
+    checkout_value_id: string;
+    cost_in_cents: number;
+    name: string;
+    value_in_cents: number;
+}
+
+
+const CheckoutButton: React.FC<CheckoutRedemptionProps> = ({ selectedValue, giftCardName }): React.ReactElement => {
     const buttonText = 'Prizeout Gift Card';
-    const buttonHandler = () => {
-        // Checkout logic here
+
+    const mockApiCall = (payload:Payload ) => {
+        return Promise.resolve({
+          data: {
+            message: 'Success!',
+            payload: payload,
+          },
+        });
+      };
+
+    const buttonHandler = async () => {
+        try {
+            const response = await mockApiCall({
+              checkout_value_id: selectedValue?.checkout_value_id,
+              cost_in_cents: selectedValue?.cost_in_cents,
+              name: giftCardName,
+              value_in_cents: selectedValue?.value_in_cents
+            });
+        
+            // Handle successful response
+            console.log(response.data.message);
+            alert(JSON.stringify(response.data.payload));
+
+          } catch (error) {
+            // Handle error
+            console.error(error);
+          }
     };
 
     return (
